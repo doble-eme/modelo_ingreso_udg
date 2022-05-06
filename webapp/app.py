07033abd9af5ef71ae26004f1475db6d9a93cca3
Mario Morales <lmmoralesl@gmail.com>
2022-05-06 16:34:46 -0500

    Add logic to select data instead of input data

--- webapp/app.py
+++ webapp/app.py
@@ -1,32 +1,54 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 import pickle
 import pandas as pd
+import csv
 
-app = Flask(__name__) #Initialize the flask App
-model = pickle.load(open('modelo_udg.pkl', 'rb')) # loading the trained model
+#Initialize Flask app
+app = Flask(__name__)
 
-@app.route('/') # Homepage
-def home():
-    return render_template('index.html')
+#Loads the trained model
+model = pickle.load(open('modelo_udg.pkl', 'rb'))
 
-@app.route('/predict',methods=['POST'])
-def predict():
+@app.route("/", methods = ['GET', 'POST']) # Homepage and data read
+def index():
     '''
-    For rendering results on HTML GUI
+    Reads data from csv file
     '''
-    CALENDARIO = request.form['CALENDARIO']
-    PUNTAJE_MINIMO = request.form['PUNTAJE_MINIMO']
-    CUPO = request.form['CUPO']
-    ADMITIDOS = request.form['ADMITIDOS']
-    ASPIRANTES = request.form['ASPIRANTES']
-    # retrieving values from form
-    input_variables = pd.DataFrame([[CALENDARIO, PUNTAJE_MINIMO, CUPO, ADMITIDOS, ASPIRANTES]],
-                                        columns=['CALENDARIO', 'PUNTAJE_MINIMO', 'CUPO', 'ADMITIDOS', 'ASPIRANTES'],
-                                        dtype=float,
-                                        index=['input'])
+    with open('.data/PuntajesMinimos_last.csv') as csv_file:
+        data = csv.reader(csv_file, delimiter=',')
+        carreras = []
+        for row in data:
+            carreras.append({
+                "CALENDARIO": row[0],
+                "CENTRO": row[1],
+                "CARRERA": row[2],
+                "PUNTAJE_MINIMO": row[3],
+                "CUPO": row[4],
+                "ADMITIDOS": row[5],
+                "ASPIRANTES": row[6]
+            })
 
-    prediction = model.predict(input_variables)[0] # making prediction
+    return render_template("index.html", carreras=carreras)
+
+@app.route("/select_value", methods=['GET','POST'])
+def select_value():
+    '''
+    Reads the selection from the dropdown menu
+    '''
+    seleccion = request.form.get('seleccion_carrera')
+        
+    #Clean list to remove white spaces and new lines
+    seleccion_nnl = seleccion.replace('\r\n','')
+    selection_clean = seleccion_nnl.replace(' ','')
+    seleccion_list = selection_clean.split(',')
+        
+    #Creates the data frame to be sent to the predicion model
+    input_variables = pd.DataFrame(seleccion_list).T
+
+    #Executes the prediction using the trained model
+    # print(input_variables)
+    prediction = model.predict(input_variables)[0]
 
     #if statement to determine if the probability is high or low according to the prediction result
     if prediction == 0:
@@ -34,8 +56,8 @@ def predict():
     elif prediction == 1:
         result = 'Probabilidad alta'
 
-    return render_template('index.html', prediction_text=result) # rendering the predicted result on the page
-
+    # rendering the predicted result on the page
+    return render_template('index.html', prediction_text=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
